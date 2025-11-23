938f43ecfd36f4389db939ff519f18db039dbedf
Mauro Minella <mauro.minella@microsoft.com>
2025-11-23 16:21:18 +0100

    maf

--- python/maf02_responses/README.md
+++ python/maf02_responses/README.md
@@ -14,6 +14,13 @@
   - on Windows --> `.\.venv\Scripts\activate.ps1`.
 - To deactivate --> `deactivate`.
 
+## What this sample does
+This sample creates an agent with the Microsoft Agent Framework that relies on the Azure OpenAI Responses service.<br/>
+It uses the direct endpoint -like `https://my-azure-openai-resource.openai.azure.com/` of an Azure OpenAI resource.<br/>
+- First, it creates the client -`AzureOpenAIResponsesClient`- to the Azure OpenAI service that is responses-API enabled.
+- Then, it uses the client to create the agent.
+- Optionally, it can engage AssistantApi-like tools like *CodeInterpreter* and *FileSearch*.
+
 ## Reference docs
 - [Azure OpenAI Responses Agents](https://learn.microsoft.com/en-us/agent-framework/user-guide/agents/agent-types/azure-openai-responses-agent?pivots=programming-language-python)
 - This sample adopts [Azure OpenAI APIs next generation v1](https://learn.microsoft.com/en-us/azure/ai-foundry/openai/api-version-lifecycle?view=foundry-classic&tabs=python#api-evolution)
--- python/maf02_responses/main.py
+++ python/maf02_responses/main.py
@@ -5,35 +5,22 @@ from agent_framework.azure import AzureOpenAIResponsesClient
 from azure.identity import AzureCliCredential, DefaultAzureCredential, get_bearer_token_provider
 from openai import OpenAI
 
-# Azure OpenAI APIs next generation v1 example
-async def basic_example_v1():
+async def basic_agent():
     # Create an agent using Azure OpenAI Responses
-    token_provider = get_bearer_token_provider(
-        DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
-    )
-    client = OpenAI(  
-    base_url = f"{os.getenv("AZURE_OPENAI_ENDPOINT")}openai/v1/",  
-    api_key = token_provider  
-    )
-    response = client.responses.create(
-        model="gpt-4.1",
-        input= "This is a test" 
-    )
-    print(response.output_text) # response.model_dump_json(indent=2))
-
-
-async def basic_example():
-    # Create an agent using Azure OpenAI Responses
-    agent = AzureOpenAIResponsesClient(
+    openairesponses_client = AzureOpenAIResponsesClient(
         credential=AzureCliCredential(),
         endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
         deployment_name = os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"),
-        api_version = os.getenv("AZURE_OPENAI_RESPONSES_API_VERSION"),
-    ).create_agent(
+        api_version = os.getenv("AZURE_OPENAI_RESPONSES_API_VERSION"), # v1
+    )
+
+    openai_agent = openairesponses_client.create_agent(
         name="HelpfulAssistant",
         instructions="You are good at telling jokes.",
     )
-    result = await agent.run("Tell me a joke about a pirate.")
+
+    result = await openai_agent.run("Tell me a joke about a pirate.")
+    
     print(result.text)
 
 
@@ -46,7 +33,7 @@ def main():
         print("Environment variables have been loaded ;-)")
 
     print("Hello from maf02-responses!")
-    asyncio.run(basic_example_v1())
+    asyncio.run(basic_agent())
 
 if __name__ == "__main__":
     main()
